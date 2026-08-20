e318d141aa640c1ad62593bd9972a01eb8828e61
Arash <Safari.arash@gmail.com>
2026-08-20 20:49:14 +0200

    Build Header component

---
+++ app/components/Header/index.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="w-full max-w-3xl mx-auto mb-10 rounded-2xl border-2 border-purple-500 bg-slate-900 px-6 py-8 text-center">
+      <h1 className="text-4xl font-bold text-purple-300">Neon Reflex</h1>
+
+      <h2 className="mt-2 text-lg text-purple-200">Click the correct square</h2>
+    </header>
+  );
+};
+
+export default Header;
