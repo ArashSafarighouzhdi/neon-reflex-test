c6af907c120ab646424fa3cb96465b41ab38d009
Arash <Safari.arash@gmail.com>
2026-08-20 21:47:24 +0200

    Edit header base on tests

--- app/components/Subtitle/index.tsx
+++ app/components/Subtitle/index.tsx
@@ -1,8 +1,6 @@
 const Subtitle = () => {
   return (
-    <header className="w-full max-w-3xl mx-auto mb-10 rounded-2xl border-2 border-purple-500 bg-slate-900 px-6 py-8 text-center">
-      <h2 className="mt-2 text-lg text-purple-200">Click the correct square</h2>
-    </header>
+    <h2 className="mt-2 text-lg text-purple-200">Click the correct square</h2>
   );
 };
 
